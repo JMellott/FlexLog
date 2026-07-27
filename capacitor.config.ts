6b152584367e4a9a6a8761dad00e59697182b600
JMellott <mellott.joshua@gmail.com>
2026-07-27 17:06:29 -0500

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.flexlog.app',
-  appName: 'FlexLog',
+  appId: 'com.jmellott.flexlog',
+  appName: 'FlexLogger',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
